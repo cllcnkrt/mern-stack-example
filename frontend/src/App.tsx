2d5276c058a37ebec1ed25465567fc64c0b5da6d
cllcnkrt <cllcnkrt@outlook.com>
2022-10-16 19:03:00 +0300

    routes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login, Register, Dashboard } from './pages';
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
