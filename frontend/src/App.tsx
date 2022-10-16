8828a2f4f6aba28a968a9b035e77f05bb5f64114
cllcnkrt <cllcnkrt@outlook.com>
2022-10-16 19:21:22 +0300

    Header component completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login, Register, Dashboard } from './pages';
+import { Header } from './components';
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
